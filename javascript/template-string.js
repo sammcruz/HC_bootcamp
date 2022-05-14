6a4666d73dbc9e3b824e7237804ab16027a642fd
sammcruz <samantha@poli.ufrj.br>
2022-05-14 17:33:31 -0300

    template string

---
+++ javascript/template-string.js
@@ -0,0 +1,25 @@
+
+
+let nome = 'Samantha';
+let sobrenome = 'Cruz';
+let profissao = 'Developer'
+
+// usando o console com aspas simples
+console.log(
+    'Olá, meu nome é ' +
+    nome +
+    ' ' +
+    sobrenome +
+    ' e minha profissão é: ' +
+    profissao
+)
+
+// usando o símbolo da crase
+console.log(`Olá, meu nome é ${nome} ${sobrenome} e minha profissão é: ${profissao}`)
+console.log(`A some de 1 + 1 é: ${1+1}`)
+console.log(`O objeto json: ${{ chave: 'valor'}}`)
+
+let meuObjeto = {
+    chave: 'novo valor'
+}
+
